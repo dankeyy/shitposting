be6982e5f6fc0e453373e0a696bcdd09cc51847d
dankeyy <dankeyy@protonmail.com>
2022-09-08 12:17:05 +0300

    til how to remove repeating duplicate characters from string

---
+++ remove_repeating.py
@@ -0,0 +1,22 @@
+# yes sir im a snek programmer
+
+def f(s):
+ i = 0;
+ ss = '';
+ while isinstance(i := next(((j+1, ss := ss + s[j]) for j in range(i, len(s)) if i == 0 or s[j] != ss[-1]), (ss, print('rekt')))[0], int):...;
+ return ss;
+
+
+print("%s" % f("abbcccddddHeeyykkkkek"));
+
+
+# output:
+# rekt
+# rekt
+# rekt
+# rekt
+# rekt
+# rekt
+# rekt
+# rekt
+# abcdHeykek
