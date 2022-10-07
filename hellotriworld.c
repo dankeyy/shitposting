c356a380b2a34168b2becb7ba3df27b78a0ff170
dankeyy <dankeyy@protonmail.com>
2022-10-07 15:34:43 +0300

    make it pretty

--- hellotriworld.c
+++ hellotriworld.c
@@ -3,10 +3,10 @@ stdio.h>
 
 int main(int argc,char *argv??(??))
 <% char poo<:0xF:>= ??<'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'%>;
-// do you know da wae??/
+  // /do you know da wae??/
   printf("%sike", lololol);
   printf("%s??/n", poo);
-  return ??--1??!1?0:1; ??>
+  return ??--1??!1?1-1:1; ??>
 
 // hello world
 
