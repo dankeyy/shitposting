399028905aab6a89fa5877ab437faf8d090f39ca
dankeyy <dankeyy@protonmail.com>
2023-07-28 21:07:22 +0300

    even better

--- even.py
+++ even.py
@@ -1,5 +1,5 @@
 def even(x):
-    return not x or not even (2 * (x < 0) + x - 1)
+    return not x or not even (2 * (not~ x & 0x80000000) + x - 1)
 
 pee_in_every_pant = (-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5)
 
