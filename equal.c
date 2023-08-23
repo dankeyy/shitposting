b15f291cd58e1bd380fbb640099d4ae80f6799d8
dankeyy <dankeyy@protonmail.com>
2023-08-23 23:34:21 +0300

    swim through the sea of UB

--- equal.c
+++ equal.c
@@ -8,20 +8,24 @@
 // c is fun
 
 eql (char *a, char *b) {
-    (a == b) || ({while (*a && *b && *a++ == *b++); !*a && !*b;});
+    (a == b) + (!a && !b) || (a && b && ({while (*a++ && *b++ && *a == *b); !*a && !*b;}));
 }
 
 
 int main() {
-    char* a = "lolol";
-    char* b = "lolol";
-    char* c = a;
-    char* d = "kek";
-    char* s = "s";
+    char* a = "xyz";
+    char* b = "xyz";
+    char* c = "xym";
+    char* d = "myz";
+    char* e = "m";
+    char* f = NULL;
+    char* g = NULL;
 
     printf(eql(a, b) ? "true\n" : "false\n");
     printf(eql(a, c) ? "true\n" : "false\n");
     printf(eql(a, d) ? "true\n" : "false\n");
-    printf(eql(a, s) ? "true\n" : "false\n");
-    printf(eql(c, b) ? "true\n" : "false\n");
+    printf(eql(a, e) ? "true\n" : "false\n");
+    printf(eql(a, f) ? "true\n" : "false\n");
+    printf(eql(c, d) ? "true\n" : "false\n");
+    printf(eql(f, g) ? "true\n" : "false\n");
 }
